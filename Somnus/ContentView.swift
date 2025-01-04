63e2558336e7e131cb877cd408c0985148b406d7
Harlech1 <turkerkizilcik@gmail.com>
2025-01-04 17:41:55 +0300

    lfg

---
+++ Somnus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Somnus
+//
+//  Created by Türker Kızılcık on 4.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Somnus/SomnusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomnusApp.swift
+//  Somnus
+//
+//  Created by Türker Kızılcık on 4.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SomnusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
