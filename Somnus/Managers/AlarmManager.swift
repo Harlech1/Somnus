84383acb9d0a1b98c6fa22b54d144ee9dfd13265
Harlech1 <turkerkizilcik@gmail.com>
2025-01-07 17:36:22 +0300

    Add: silent music works for background awakening

--- Somnus/Managers/AlarmManager.swift
+++ Somnus/Managers/AlarmManager.swift
@@ -10,11 +10,13 @@ class AlarmManager: ObservableObject {
     private var volumeView: MPVolumeView?
     private var volumeSlider: UISlider?
     private var audioPlayer: AVAudioPlayer?
+    private var silentAudioPlayer: AVAudioPlayer?
     private var notificationDelegate: NotificationDelegate?
     
     init() {
         setupVolumeControl()
         setupAudioSession()
+        setupSilentAudio()
         requestNotificationPermission()
         loadAlarms()
         setupNotificationHandling()
@@ -32,7 +34,7 @@ class AlarmManager: ObservableObject {
             
             for subview in view.subviews {
                 if let slider = subview as? UISlider {
-                    slider.value = 1.0
+                    slider.value = 0.2
                     volumeSlider = slider
                     break
                 }
@@ -43,9 +45,9 @@ class AlarmManager: ObservableObject {
     private func setupAudioSession() {
         do {
             let audioSession = AVAudioSession.sharedInstance()
-            try audioSession.setCategory(.playback, mode: .default, options: [])
-            try audioSession.setActive(true)
-
+            try audioSession.setCategory(.playback, mode: .default, options: [.mixWithOthers, .duckOthers])
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            
             if let soundURL = Bundle.main.url(forResource: "alarm_sound", withExtension: "wav") {
                 audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
                 audioPlayer?.numberOfLoops = -1
@@ -56,15 +58,31 @@ class AlarmManager: ObservableObject {
         }
     }
     
+    private func setupSilentAudio() {
+        do {
+            if let silentURL = Bundle.main.url(forResource: "silence", withExtension: "mp3") {
+                silentAudioPlayer = try AVAudioPlayer(contentsOf: silentURL)
+                silentAudioPlayer?.numberOfLoops = -1  // Loop indefinitely
+                silentAudioPlayer?.volume = 0.0       // Make sure it's silent
+                silentAudioPlayer?.play()             // Start playing immediately
+            }
+        } catch {
+            print("Failed to setup silent audio: \(error)")
+        }
+    }
+    
     func maximizeVolume() {
         DispatchQueue.main.async {
-            self.volumeSlider?.setValue(1.0, animated: false)
+            self.volumeSlider?.setValue(0.2, animated: false)
             self.volumeSlider?.sendActions(for: .touchUpInside)
         }
     }
     
     func playAlarmSound() {
         do {
+            // Pause silent audio while alarm is playing
+            silentAudioPlayer?.pause()
+            
             try AVAudioSession.sharedInstance().setActive(true)
             audioPlayer?.play()
         } catch {
@@ -74,6 +92,8 @@ class AlarmManager: ObservableObject {
     
     func stopAlarmSound() {
         audioPlayer?.stop()
+        // Resume silent audio
+        silentAudioPlayer?.play()
     }
     
     func setupNotificationHandling() {
@@ -191,5 +211,11 @@ class AlarmManager: ObservableObject {
         let identifiers = (0..<maxNotifications).map { "\(alarm.id.uuidString)-\($0)" }
         UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: identifiers)
         stopAlarmSound()
+    }
+    
+    deinit {
+        // Clean up audio players
+        silentAudioPlayer?.stop()
+        audioPlayer?.stop()
     }
 } 
